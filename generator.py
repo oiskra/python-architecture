cc176381bfd6cf60b301ff55fac5fa772217b00b
Olaf <ojaskra@gmail.com>
2024-06-11 13:09:51 +0200

    completed generator task

--- generator.py
+++ generator.py
@@ -6,7 +6,18 @@ class NToPowerGenerator:
         self.n = n
     
     def __iter__(self):
+        self.current = 0
         return self
     
     def __next__(self):
-        pass
+        self.current += 1
+        if(self.current >= self.n):
+            raise StopIteration      
+        
+        return self.a ** self.current
+    
+    
+x = NToPowerGenerator(2, 10)
+
+for i in iter(x):
+    print(i)
