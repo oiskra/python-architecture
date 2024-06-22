b3c125c8b9beb275ab7cb59ebe7b7f944d97572e
Olaf <ojaskra@gmail.com>
2024-06-22 14:57:12 +0200

    deleted comment from task 5

--- cdCollection.py
+++ cdCollection.py
@@ -1,8 +1,3 @@
-# Korzystając z danych XML:
-#  https://www.w3schools.com/xml/cd_catalog.xml
-#  zawierających kolekcję płyt CD, opracuj program, który wyświetli zestaw utworów i
-#  wykonawców w postaci listy: [(wykonawca1, tytuł1), …]. 
-
 import requests
 import xml.etree.ElementTree as ET
 
