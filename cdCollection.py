2aa46b52f75d5832d569dc340afc7654bd6e52a6
Olaf <ojaskra@gmail.com>
2024-06-22 14:56:37 +0200

    finished task 5

---
+++ cdCollection.py
@@ -0,0 +1,31 @@
+# Korzystając z danych XML:
+#  https://www.w3schools.com/xml/cd_catalog.xml
+#  zawierających kolekcję płyt CD, opracuj program, który wyświetli zestaw utworów i
+#  wykonawców w postaci listy: [(wykonawca1, tytuł1), …]. 
+
+import requests
+import xml.etree.ElementTree as ET
+
+URL = "https://www.w3schools.com/xml/cd_catalog.xml"
+
+if __name__ == "__main__":
+    results = []
+    response = requests.get(URL)
+    root = ET.fromstring(response.content)    
+    
+    for i in root.findall("CD"):
+        title = i.find("TITLE").text
+        artist = i.find("ARTIST").text
+        results.append((artist, title))
+        
+    print(results)
+        
+        
+# <CD>
+#     <TITLE>Empire Burlesque</TITLE>
+#     <ARTIST>Bob Dylan</ARTIST>
+#     <COUNTRY>USA</COUNTRY>
+#     <COMPANY>Columbia</COMPANY>
+#     <PRICE>10.90</PRICE>
+#     <YEAR>1985</YEAR>
+# </CD>
--- xml.py
+++
@@ -1,4 +0,0 @@
-# Korzystając z danych XML:
-#  https://www.w3schools.com/xml/cd_catalog.xml
-#  zawierających kolekcję płyt CD, opracuj program, który wyświetli zestaw utworów i
-#  wykonawców w postaci listy: [(wykonawca1, tytuł1), …].
