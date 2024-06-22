4a5ef8cace3eff5c8dacf04a76cb56f133a76da6
Olaf <ojaskra@gmail.com>
2024-06-22 11:27:19 +0200

    init task 4, 5, 6

---
+++ bankaccount.py
@@ -0,0 +1,5 @@
+# Napisz klasę BankAccount, która implementuje podstawowe operacje na koncie
+#  bankowym, takie jak wpłacanie, wypłacanie i sprawdzanie salda. Klasa powinna
+#  wywoływać wyjątek przy próbie wpłaty lub wypłaty niepoprawnej kwoty. Następnie napisz
+#  testy jednostkowe za pomocą PyTest, które sprawdzą poprawność działania metod oraz
+#  obsługę błędów.
---
+++ xml.py
@@ -0,0 +1,4 @@
+# Korzystając z danych XML:
+#  https://www.w3schools.com/xml/cd_catalog.xml
+#  zawierających kolekcję płyt CD, opracuj program, który wyświetli zestaw utworów i
+#  wykonawców w postaci listy: [(wykonawca1, tytuł1), …].
